0403503a994a45adab3e9de02b17cd5ac77ae864
Alex Zhidkov <AlexZhidkov@users.noreply.github.com>
2017-03-15 17:56:17 +0800

    three way binding example

--- src/app/legal-advice/legal-advice.component.ts
+++ src/app/legal-advice/legal-advice.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFire, FirebaseObjectObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-legal-advice',
   templateUrl: './legal-advice.component.html',
-  styleUrls: ['./legal-advice.component.css']
+  styleUrls: ['./legal-advice.component.css'],
 })
-export class LegalAdviceComponent implements OnInit {
 
-  constructor() { }
+export class LegalAdviceComponent implements OnInit {
+  item: FirebaseObjectObservable<any>;
+  constructor(af: AngularFire) {
+    this.item = af.database.object('/relationship/dev-data');
+  }
 
   ngOnInit() {
   }
