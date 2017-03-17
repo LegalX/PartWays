a3eb62bc574d2151fa8867c52453da0778710e72
rkalita <exit2nowhere@gmail.com>
2017-03-17 10:00:29 +0700

    Develop Part D About the children

--- src/app/children/children.component.ts
+++ src/app/children/children.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFire, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-children',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./children.component.css'],
 })
 export class ChildrenComponent implements OnInit {
+  items: FirebaseListObservable<any>;
+  children: Array<FirebaseObjectObservable<any>>;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private route: ActivatedRoute, private af: AngularFire) {
+    this.items = this.af.database.list(`/children/dev-data`);
   }
 
+  addChild() {
+    if (confirm('Do you want to add new child?')) {
+      this.items.push({}).then((item) => {
+        this.children.push(this.af.database.object(`/children/dev-data/` + item.key));
+      });
+    }
+  }
+
+  removeChild(id: string) {
+    if (confirm('Are you sure?')) {
+      this.children[id].remove();
+      delete this.children[id];
+    }
+  }
+
+  ngOnInit() {
+    this.children = this.route.snapshot.data['children'];
+  }
 }
--- src/app/children/children.resolver.service.ts
+++ src/app/children/children.resolver.service.ts
@@ -7,12 +7,20 @@ import { Observable } from 'rxjs/Observable';
 @Injectable()
 export class ChildrenResolver implements Resolve<any> {
 
-    constructor(private af: AngularFire) { }
+  constructor(private af: AngularFire) { }
 
-    resolve(route: ActivatedRouteSnapshot): Observable<any> | Promise<any> {
-        const data = this.af.database.object(`/children/dev-data`);
-        return data.map((item) => {
-            return item;
-        }).first();
-    }
+  resolve(route: ActivatedRouteSnapshot): Observable<any> | Promise<any> {
+    const data = this.af.database.object(`/children/dev-data`);
+    return data
+      .map((children) => {
+        let childKeys = Object.keys(children);
+        return childKeys.map((childKey) => {
+          return this.af.database.object(`/children/dev-data/` + childKey)
+            .map((it) => {
+              return it;
+            }).first();
+        });
+      }).first()
+    ;
+  }
 }
