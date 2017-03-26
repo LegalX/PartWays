9f89cb4b1547839d5aa6c102c43295f4c1b751fa
Alexander Zhidkov <Alexander Zhidkov>
2017-03-26 12:53:09 +0800

    added sendEmailConfirmation function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,56 @@
-var functions = require('firebase-functions');
+/**
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// })
+const functions = require('firebase-functions');
+const nodemailer = require('nodemailer');
+// Configure the email transport using the default SMTP transport and a GMail account.
+// For other types of transports such as Sendgrid see https://nodemailer.com/transports/
+// TODO: Configure the `gmail.email` and `gmail.password` Google Cloud environment variables.
+const gmailEmail = 'partways.dev@gmail.com'; // encodeURIComponent(functions.config().gmail.email);
+const gmailPassword = encodeURIComponent(functions.config().gmail.password);
+const mailTransport = nodemailer.createTransport(`smtps://${gmailEmail}:${gmailPassword}@smtp.gmail.com`);
+
+// Sends an email confirmation when a user changes his mailing list subscription.
+exports.sendEmailConfirmation = functions.database.ref('/users/{uid}').onWrite(event => {
+    const snapshot = event.data;
+    const val = snapshot.val();
+
+    if (!snapshot.changed('subscribedToMailingList')) {
+        return;
+    }
+
+    const mailOptions = {
+        from: '"PartWays" <noreply@partways.com>',
+        to: val.email
+    };
+
+    // The user just subscribed to our newsletter.
+    if (val.subscribedToMailingList) {
+        mailOptions.subject = 'Thanks and Welcome!';
+        mailOptions.text = 'Thanks you for subscribing to our newsletter. You will receive our next weekly newsletter.';
+        return mailTransport.sendMail(mailOptions).then(() => {
+            console.log('New subscription confirmation email sent to:', val.email);
+        });
+    }
+
+    // The user unsubscribed to the newsletter.
+    mailOptions.subject = 'Sad to see you go :`(';
+    mailOptions.text = 'I hereby confirm that I will stop sending you the newsletter.';
+    return mailTransport.sendMail(mailOptions).then(() => {
+        console.log('New unsubscription confirmation email sent to:', val.email);
+    });
+});
