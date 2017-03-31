5a95bbd46d063c5203f97ed5b47f32e92b362150
Alex Zhidkov <AlexZhidkov@users.noreply.github.com>
2017-03-31 14:09:17 +0800

    Added  custom launcher: Chrome_travis_ci

--- karma.conf.js
+++ karma.conf.js
@@ -2,7 +2,7 @@
 // https://karma-runner.github.io/0.13/config/configuration-file.html
 
 module.exports = function (config) {
-  config.set({
+  var configuration = {
     basePath: '',
     frameworks: ['jasmine', '@angular/cli'],
     plugins: [
@@ -34,6 +34,12 @@ module.exports = function (config) {
     reporters: config.angularCli && config.angularCli.codeCoverage
               ? ['progress', 'coverage-istanbul']
               : ['progress', 'kjhtml'],
+    customLaunchers: {  
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
@@ -41,5 +47,11 @@ module.exports = function (config) {
     browsers: ['Chrome'],
     singleRun: false,
     browserNoActivityTimeout: 100000
-  });
+  };
+  
+  if(process.env.TRAVIS) {  
+    config.browsers = ['Chrome_travis_ci'];
+  }
+  
+  config.set(configuration);
 };
