929ccc8f67febe867a239137e9f5c04aad20cf43
Alex Zhidkov <AlexZhidkov@users.noreply.github.com>
2017-04-06 11:11:01 +0800

    ChatService should get answers test

--- src/app/chat/chat.service.spec.ts
+++ src/app/chat/chat.service.spec.ts
@@ -1,15 +1,59 @@
-import { inject, TestBed } from '@angular/core/testing';
+import { async, getTestBed, inject, TestBed } from '@angular/core/testing';
+import {
+  BaseRequestOptions, Headers,
+  Http, HttpModule, RequestMethod, Response, ResponseOptions, XHRBackend,
+} from '@angular/http';
+import { MockBackend, MockConnection } from '@angular/http/testing';
 
 import { ChatService } from './chat.service';
 
 describe('ChatService', () => {
+  let mockBackend: MockBackend;
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [ChatService],
+      imports: [HttpModule],
+      providers: [
+        ChatService,
+        MockBackend,
+        BaseRequestOptions,
+        {
+          provide: Http,
+          deps: [MockBackend, BaseRequestOptions],
+          useFactory:
+          (backend: XHRBackend, defaultOptions: BaseRequestOptions) => {
+            return new Http(backend, defaultOptions);
+          },
+        },
+      ],
+    });
+    mockBackend = getTestBed().get(MockBackend);
+  });
+
+  it('should exist', inject([ChatService], (service: ChatService) => {
+    expect(service).toBeTruthy();
+  }));
+
+  it('should get answers', (done) => {
+    let chatService: ChatService;
+
+    getTestBed().compileComponents().then(() => {
+      mockBackend.connections.subscribe(
+        (connection: MockConnection) => {
+          connection.mockRespond(new Response(
+            new ResponseOptions({
+              body: { answer: 'Sample response', score: '100' },
+            },
+            )));
+        });
+
+      chatService = getTestBed().get(ChatService);
+      expect(chatService).toBeDefined();
+
+      chatService.getAnswer('test').subscribe((answer: string) => {
+        expect(answer).toBe('Sample response');
+        done();
+      });
     });
   });
 
-  it('should ...', inject([ChatService], (service: ChatService) => {
-    expect(service).toBeTruthy();
-  }));
 });
