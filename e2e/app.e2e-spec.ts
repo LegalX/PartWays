6ff8453fe68fce957a2025efcfad8fe5708f018e
Alex Zhidkov <AlexZhidkov@users.noreply.github.com>
2017-04-06 17:11:58 +0800

    Added e2e tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,17 @@
 import { PartWaysPage } from './app.po';
 
-describe('part-ways App', () => {
+describe('PartWays App', () => {
   let page: PartWaysPage;
 
   beforeEach(() => {
     page = new PartWaysPage();
   });
 
-  it('should display message saying app works', () => {
+  it('should have core buttons on landing page', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual(true);
+    expect(page.getButton('PartWays')).toEqual(true);
+    expect(page.getButton('Google')).toEqual(true);
+    expect(page.getButton('Facebook')).toEqual(true);
+    expect(page.getElementById('title-button')).toEqual(true);
   });
 });
