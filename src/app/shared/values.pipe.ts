a90a17f356b0e44d757775714e175ba159b622f6
Alex Zhidkov <AlexZhidkov@users.noreply.github.com>
2017-04-21 10:59:21 +0800

    Fixing pipe arguments from array to values

--- src/app/shared/values.pipe.ts
+++ src/app/shared/values.pipe.ts
@@ -2,17 +2,17 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({ name: 'values' })
 export class ValuesPipe implements PipeTransform {
-  transform(value: any, args?: any[]): Object[] {
+  transform(value: any, arg1: any, arg2?: any): Object[] {
     const keyArr: any[] = Object.keys(value);
     const dataArr = [];
-    const keyName = args[0];
+    const keyName = arg1;
 
     keyArr.forEach((key: any) => {
       value[key][keyName] = key;
       dataArr.push(value[key]);
     });
 
-    if (args[1]) {
+    if (arg2) {
       dataArr.sort((a: Object, b: Object): number => {
         return a[keyName] > b[keyName] ? 1 : -1;
       });
