019821929cc09c52bdbd100ea9bb62bc5d6df88b
vidur <vidursingla13@gmail.com>
2017-04-28 18:40:03 +0530

    improved login experiene

--- src/app/auth/login.ts
+++ src/app/auth/login.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
+import { AngularFire, AuthMethods, AuthProviders, FirebaseListObservable } from 'angularfire2';
 
 @Component({
     selector: 'app-login',
     templateUrl: './login.html',
 })
 
-export class LoginComponent { }
+export class LoginComponent {
+
+  constructor(public af: AngularFire) { }
+  loginGoogle() {
+    this.af.auth.login();
+  }
+
+  loginFacebook() {
+    this.af.auth.login({
+      provider: AuthProviders.Facebook,
+      method: AuthMethods.Redirect,
+    });
+  }
+}
