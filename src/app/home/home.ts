279caeffe8930420406677e7352078d795f30358
Alexander Zhidkov <Alexander Zhidkov>
2017-05-28 12:12:28 +0800

    #165 Add image to home page

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-home',
     templateUrl: './home.html',
+    styleUrls: ['./home.css'],
 })
 
 export class HomeComponent { }
